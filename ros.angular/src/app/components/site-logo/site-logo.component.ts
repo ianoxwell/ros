a7b7edb0b556a0520e75838501ef01fe881d3d6c
ianOxRoller <135286319+ianOxRoller@users.noreply.github.com>
2025-02-26 16:38:57 +1000

    feat: site logo decrease dependency on common.scss

--- ros.angular/src/app/components/site-logo/site-logo.component.ts
+++ ros.angular/src/app/components/site-logo/site-logo.component.ts
@@ -4,6 +4,5 @@ import { Component } from '@angular/core';
     selector: 'app-site-logo',
     templateUrl: './site-logo.component.html',
     styleUrls: ['./site-logo.component.scss'],
-    standalone: false
 })
 export class SiteLogoComponent {}
