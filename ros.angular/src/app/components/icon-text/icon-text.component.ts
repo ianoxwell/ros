667b7ad8227b362f077177a0d819ff511b10a252
ianOxRoller <135286319+ianOxRoller@users.noreply.github.com>
2025-03-01 10:24:03 +1000

    feat: allow icon-right

--- ros.angular/src/app/components/icon-text/icon-text.component.ts
+++ ros.angular/src/app/components/icon-text/icon-text.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 
@@ -5,11 +6,12 @@ import { MatIconModule } from '@angular/material/icon';
     selector: 'app-icon-text',
     templateUrl: './icon-text.component.html',
     styleUrls: ['./icon-text.component.scss'],
-    imports: [MatIconModule]
+    imports: [CommonModule, MatIconModule]
 })
 export class IconTextComponent {
   @Input() text = '';
   @Input() icon = '';
   @Input() titleText = '';
   @Input() color = '';
+  @Input() iconLeft = true;
 }
