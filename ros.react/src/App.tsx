5480a0238a98b8f80235e50379dfaa56ec0cce91
ianOxRoller <135286319+ianOxRoller@users.noreply.github.com>
2025-03-11 18:37:19 +1000

    feat: add in ability for notifications

--- ros.react/src/App.tsx
+++ ros.react/src/App.tsx
@@ -1,24 +1,34 @@
 import { MantineProvider } from '@mantine/core';
 import '@mantine/core/styles.css';
+import { Notifications } from '@mantine/notifications';
+import Login from '@pages/register/Login';
 import { Route, Routes } from 'react-router-dom';
 import './App.scss';
-import { GlobalNavigation } from './features/global-navigation/global-navigation.component';
-import { Ingredients } from './pages/ingredients';
-import NotFoundErrorPage from './pages/not-found-error';
-import { Orders } from './pages/orders';
-import { Recipes } from './pages/recipes';
-import { Schedules } from './pages/schedules';
+import { GlobalNavigation } from './features/global-navigation/GlobalNavigation.component';
+import { Ingredients, NotFoundErrorPage, Orders, Recipes, Schedules } from './pages';
 
 function App() {
   return (
     <>
       <MantineProvider>
+        <Notifications position="top-center" limit={5} autoClose={7000} />
+        {/* Note guide to mantine notifications - https://mantine.dev/x/notifications/#functions */}
         <GlobalNavigation />
         <Routes>
+          {/* <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <SharedLayout />
+              </ProtectedRoute>
+            }
+          > */}
           <Route path="/" element={<Recipes />} />
           <Route path="/orders" element={<Orders />} />
           <Route path="/schedule" element={<Schedules />} />
           <Route path="/ingredients" element={<Ingredients />} />
+          {/* </Route> */}
+          <Route path="/login" element={<Login />} />
           <Route path="*" element={<NotFoundErrorPage />} />
         </Routes>
       </MantineProvider>
