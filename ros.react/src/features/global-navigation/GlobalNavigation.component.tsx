fab3d37177aa401d4536ad5d5c070edf710e41a0
ianOxRoller <135286319+ianOxRoller@users.noreply.github.com>
2025-03-13 18:14:52 +1000

    feat: added logout to global nav

--- ros.react/src/features/global-navigation/GlobalNavigation.component.tsx
+++ ros.react/src/features/global-navigation/GlobalNavigation.component.tsx
@@ -1,7 +1,9 @@
+import { logoutUser } from '@features/user/userSlice';
 import { Menu } from '@mantine/core';
-import { Calendar, NotebookPen, Plus, Settings, ShoppingBasket, User } from 'lucide-react';
+import { Calendar, LogOut, NotebookPen, Plus, Settings, ShoppingBasket, User } from 'lucide-react';
 import { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { NavLink, useLocation } from 'react-router-dom';
 import './GlobalNavigation.component.scss';
 
 const links = [
@@ -16,16 +18,40 @@ export const GlobalNavigation = () => {
   const fillColor = '#128758';
   const iconSize = 24;
   const iconPlusSize = 28;
-  function doSomething() {
-    console.log('it did something');
+  const location = useLocation();
+  const dispatch = useDispatch();
+
+  function contextAwareNewItem() {
+    console.log('contextually add something', location.pathname);
   }
 
+  const logUserOut = () => {
+    dispatch(logoutUser('Logging out...'));
+  };
+
   return (
-    <header className="header">
+    <>
       {/* <div className="header-logo">
         <img src={vegetableBasket} className="header-logo__image" alt="React logo" height={'30px'} width={'30px'} />
         <h1 className="header-logo__title">Recipe Ordering Simplified</h1>
       </div> */}
+      <div className="user-menu">
+        <Menu shadow="md" width={200}>
+          <Menu.Target>
+            <button type="button" className="nav-fab">
+              <User size={iconSize} />
+            </button>
+          </Menu.Target>
+          <Menu.Dropdown>
+            <Menu.Label>Application</Menu.Label>
+            <Menu.Item leftSection={<Settings size={14} />}>Settings</Menu.Item>
+            <Menu.Item leftSection={<LogOut size={14} />} component="button" type="button" onClick={logUserOut}>
+              Logout
+            </Menu.Item>
+          </Menu.Dropdown>
+        </Menu>
+      </div>
+
       <nav className="bottom-nav">
         {navigation.map((nav) => (
           <NavLink to={nav.link} className="nav-item" key={nav.id}>
@@ -46,24 +72,11 @@ export const GlobalNavigation = () => {
         ))}
 
         <div className="nav-fab-container">
-          <button type="button" onClick={doSomething} className="nav-fab">
+          <button type="button" onClick={contextAwareNewItem} className="nav-fab">
             <Plus size={iconPlusSize} />
           </button>
         </div>
       </nav>
-      <div className="user-menu">
-        <Menu shadow="md" width={200}>
-          <Menu.Target>
-            <button type="button" className="nav-fab">
-              <User size={iconSize} />
-            </button>
-          </Menu.Target>
-          <Menu.Dropdown>
-            <Menu.Label>Application</Menu.Label>
-            <Menu.Item leftSection={<Settings size={14} />}>Settings</Menu.Item>
-          </Menu.Dropdown>
-        </Menu>
-      </div>
-    </header>
+    </>
   );
 };
