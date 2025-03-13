92877d3028cc4c043f796530c1524147ce91e671
ianOxRoller <135286319+ianOxRoller@users.noreply.github.com>
2025-03-13 18:15:10 +1000

    feat: typing to the store

--- ros.react/src/store.tsx
+++ ros.react/src/store.tsx
@@ -4,3 +4,5 @@ import { configureStore } from '@reduxjs/toolkit';
 export const store = configureStore({
   reducer: { user: userSlice }
 });
+export type RootState = ReturnType<typeof store.getState>
+
