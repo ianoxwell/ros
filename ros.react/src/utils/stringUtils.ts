f074a3b64f36ca05ff162ebb4ab621942894e078
ianOxRoller <135286319+ianOxRoller@users.noreply.github.com>
2025-03-27 16:54:02 +1000

    feat: fix whole number or to 3 digits

--- ros.react/src/utils/stringUtils.ts
+++ ros.react/src/utils/stringUtils.ts
@@ -24,3 +24,13 @@ export const sortLabels = (data: ISimpleReference[] | undefined) => {
     ?.map((item) => ({ value: item.id.toString(), label: sentenceCase(item.name) } as ComboboxItem))
     .sort((a, b) => a.label.localeCompare(b.label));
 };
+
+/** Takes the nutrient values 24.000, 0.1234 and returns '24' or '0.123' */
+export const fixWholeNumber = (value: number | string | undefined): string => {
+  if (!value || isNaN(Number(value))) {
+    return '0';
+  }
+
+  const num = Number(value);
+  return Number.isInteger(num) ? num.toString() : num.toFixed(3);
+};
