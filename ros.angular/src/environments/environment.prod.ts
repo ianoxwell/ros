eee9e797b825dda91dc005dc10ad2f5b90cdbc60
ianoxwell <ianoxwell@gmail.com>
2025-04-04 05:35:36 +1000

    fix: correction to API url

--- ros.angular/src/environments/environment.prod.ts
+++ ros.angular/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ declare const require: any;
 export const environment = {
   production: true,
   baseURL: 'https://ianoxwell.github.io/ProVisionCookbook/',
-  apiUrl: 'https://api-ros.fly.dev/',
+  apiUrl: 'https://api-ros.fly.dev',
   apiVersion: '/api/v1/',
   version: require('../../package.json').version,
   resultsPerPage: 10
