0516a829c59d7230396247b79383beecb3329c92
ianoxwell <ianoxwell@gmail.com>
2025-04-29 17:53:34 +1000

    feat: add modals provider to top level

--- ros.react/src/App.tsx
+++ ros.react/src/App.tsx
@@ -2,6 +2,7 @@ import SharedLayout from '@features/SharedLayout';
 import { colorsTuple, createTheme, MantineProvider } from '@mantine/core';
 import '@mantine/core/styles.css';
 import '@mantine/dates/styles.css';
+import { ModalsProvider } from '@mantine/modals';
 import { Notifications } from '@mantine/notifications';
 import '@mantine/notifications/styles.css';
 import '@mantine/tiptap/styles.css';
@@ -44,49 +45,50 @@ function App() {
       <MantineProvider theme={theme}>
         <Notifications position="top-center" color="accent" limit={5} autoClose={7000} zIndex={1001} />
         {/* Note guide to mantine notifications - https://mantine.dev/x/notifications/#functions */}
-
-        <Routes>
-          <Route
-            path={base}
-            element={
-              <ProtectedRoute>
-                <SharedLayout />
-              </ProtectedRoute>
-            }
-          >
-            <Route index element={<RecipesPage />} />
-            <Route path={CRoutes.orders} element={<Orders />} />
-            <Route path={CRoutes.schedule} element={<SchedulesPage />} />
-            <Route path={CRoutes.ingredients} element={<IngredientsPage />} />
+        <ModalsProvider>
+          <Routes>
             <Route
-              path={`${CRoutes.ingredients}/:id`}
+              path={base}
               element={
-                <>
-                  <IngredientsPage />
-                  <IngredientModal />
-                </>
+                <ProtectedRoute>
+                  <SharedLayout />
+                </ProtectedRoute>
               }
-            />
-            <Route
-              path={`${CRoutes.recipe}/:id`}
-              element={
-                <>
-                  <RecipesPage />
-                  <RecipeModal />
-                </>
-              }
-            />
-          </Route>
-          <Route path={base} element={<AccountWrapper />}>
-            <Route path={CRoutes.login} element={<Login />} />
-            <Route path={CRoutes.forgotPassword} element={<ForgotPassword />} />
-            <Route path={CRoutes.verifyEmail} element={<VerifyEmail />} />
-            <Route path={CRoutes.resetPassword} element={<ResetPassword />} />
-          </Route>
-          {/* TODO add in the rest of the registration bits here */}
-          {/* TODO - make sure the registration flow in the API does not send back anything useful like logging in - should show something about email should be sent... */}
-          <Route path="*" element={<NotFoundErrorPage />} />
-        </Routes>
+            >
+              <Route index element={<RecipesPage />} />
+              <Route path={CRoutes.orders} element={<Orders />} />
+              <Route path={CRoutes.schedule} element={<SchedulesPage />} />
+              <Route path={CRoutes.ingredients} element={<IngredientsPage />} />
+              <Route
+                path={`${CRoutes.ingredients}/:id`}
+                element={
+                  <>
+                    <IngredientsPage />
+                    <IngredientModal />
+                  </>
+                }
+              />
+              <Route
+                path={`${CRoutes.recipe}/:id`}
+                element={
+                  <>
+                    <RecipesPage />
+                    <RecipeModal />
+                  </>
+                }
+              />
+            </Route>
+            <Route path={base} element={<AccountWrapper />}>
+              <Route path={CRoutes.login} element={<Login />} />
+              <Route path={CRoutes.forgotPassword} element={<ForgotPassword />} />
+              <Route path={CRoutes.verifyEmail} element={<VerifyEmail />} />
+              <Route path={CRoutes.resetPassword} element={<ResetPassword />} />
+            </Route>
+            {/* TODO add in the rest of the registration bits here */}
+            {/* TODO - make sure the registration flow in the API does not send back anything useful like logging in - should show something about email should be sent... */}
+            <Route path="*" element={<NotFoundErrorPage />} />
+          </Routes>
+        </ModalsProvider>
       </MantineProvider>
     </>
   );
